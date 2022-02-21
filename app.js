f6748f833a326994199deca8f032a6f38c253e14
wuzhixiong <wuzhixiong@xmigc.com>
2022-02-21 14:25:54 +0800

    'zengjiagengxinpanduan'

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import request from './utils/request'
 App({
   onLaunch() {
+    this.autoUpdate()
     this.getReleaseCityWx()
     this.getParamConfig()
     wx.showShareMenu({
@@ -53,5 +54,65 @@ App({
       }
       this.globalData.paramConfigObj = paramConfigObj
     })
+  },
+
+  autoUpdate: function() {
+
+    var self = this
+
+    // 获取小程序更新机制兼容
+
+    if (wx.canIUse('getUpdateManager')) {
+
+      const updateManager = wx.getUpdateManager()
+
+      //1. 检查小程序是否有新版本发布
+
+      updateManager.onCheckForUpdate(function(res) {
+
+        // 请求完新版本信息的回调
+
+        if (res.hasUpdate) {
+
+          //2. 小程序有新版本，则静默下载新版本，做好更新准备
+
+          updateManager.onUpdateReady(function() {
+
+            updateManager.applyUpdate()
+
+          })
+
+          updateManager.onUpdateFailed(function() {
+
+            // 新的版本下载失败
+
+            wx.showModal({
+
+              title: '已经有新版本了哟~',
+
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
+
+            })
+
+          })
+
+        }
+
+      })
+
+    } else {
+
+      // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+
+      wx.showModal({
+
+        title: '提示',
+
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+
+      })
+
+    }
+
   }
 })
